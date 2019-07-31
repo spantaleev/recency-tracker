spantaleev/recency-tracker

commit 3c12fb8e26c283baebf8cb8160725cdea21dc2f1

Remove md5 package dependency in favor of crypto

lib/persister/filesystem.js
@@ -1,20 +1,15 @@
 const
+	crypto = require('crypto'),
 	path = require('path'),
 	fs = require('fs');
-
-let md5 = null;
-try {
-	md5 = require('md5');
-} catch (e) {
-	throw new Error('The filesystem persister requires the `md5` package (`npm install md5@2.x`).');
-}
 
 var Persister = function (config) {
 	this.config = config;
 };
 
 Persister.prototype.getResourcePath = function (resource) {
-	return path.join(this.config.storagePath, md5(resource) + '.json');
+	var name = crypto.createHash('sha256').update(resource, 'utf8').digest() + '.json';
+	return path.join(this.config.storagePath, name);
 };
 
 Persister.prototype.set = function (resource, data, cb) {

commit 15222a9b3a22734a88a328ab1ecdfd2eeb2403b8

Fix filesystem persister
Regression since 3c12fb8e26c283b.

lib/persister/filesystem.js
@@ -8,7 +8,7 @@ var Persister = function (config) {
 };
 
 Persister.prototype.getResourcePath = function (resource) {
-	var name = crypto.createHash('sha256').update(resource, 'utf8').digest() + '.json';
+	var name = crypto.createHash('sha256').update(resource, 'utf8').digest('hex') + '.json';
 	return path.join(this.config.storagePath, name);
 };
 